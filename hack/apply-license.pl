43c6b6b38954e7bde5715f23080423a228e1d3ed
Tom Peters <weters@me.com>
2026-01-27 23:03:38 -0500

    Update apply-license.pl to only require license on files that will be committed

--- hack/apply-license.pl
+++ hack/apply-license.pl
@@ -39,7 +39,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 EOF
 
-chomp( my @files = `find . \! -path '*/node_modules/*' \! -path '*/.git/*' -type f \\( -name '*.js' -o -name '*.css' -o -name '*.vue' \\)` );
+chomp( my @files = `git ls-files --cached --others --exclude-standard -- '*.js' '*.css' '*.vue'` );
 
 for my $file (@files) {
 	open my $fh, '<', $file
